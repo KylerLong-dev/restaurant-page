2785d277de129550c2aa747068a0e73ce561d427
Kyler Long <kylerlong24@gmail.com>
2024-12-18 18:57:19 -0500

    Add modules for menu and contact pages.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,5 @@ createContentText();
 createWelcome();
 createInfo();
 createGalleryTwo();
+
+
---
+++ src/menu.js
@@ -0,0 +1,8 @@
+
+const createMenu = () => {
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    menu.innerHTML = `
+    
+    `
+}
