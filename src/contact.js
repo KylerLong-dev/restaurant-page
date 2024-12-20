dc1d72d44d1f5758ed14144ea73a22b0a5946e15
Kyler Long <kylerlong24@gmail.com>
2024-12-20 17:08:05 -0500

    Add css for contact and menu pages

--- src/contact.js
+++ src/contact.js
@@ -5,12 +5,12 @@ const createContact = () => {
     contact.classList.add("contact");
     contact.innerHTML = `
     <h1>Contact Us</h1>
-    <div>
+    <div class="contact_person">
         <h2>Chef Ky</h2>
         <p>123-123-1234</p>
         <p>realemail@fake.com</p>
     </div>
-    <div>
+    <div class="contact_person">
         <h2>Owner Ty</h2>
         <p>123-123-1234</p>
         <p>realemail@fake.com</p>
--- src/menu.js
+++ src/menu.js
@@ -7,7 +7,7 @@ const createMenu = () => {
     <h1>Menu</h1>
     <div class="apps">
         <h2>Appetizers</h2>
-        <ul>
+        <ul class="list">
             <li>Egg Rolls</li>
             <li>Spring Rolls</li>
             <li>Egg Drop Soup</li>
@@ -18,7 +18,7 @@ const createMenu = () => {
     
     <div class="sushi">
         <h2>Sushi</h2>
-        <ul>
+        <ul class="list">
             <li>Green Dragon Roll</li>
             <li>Caterpillar Roll</li>
             <li>Central Roll</li>
@@ -28,7 +28,7 @@ const createMenu = () => {
 
     <div class="dessert">
         <h2>Dessert</h2>
-        <ul>
+        <ul class="list">
             <li>Fried Ice Cream</li>
             <li>Green Tea Ice Cream</li>
         </ul>
